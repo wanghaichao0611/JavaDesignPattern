76623d9e4f3ce1f5a06863b7c50b964cb36fe6f3
汪海潮 <949234299@qq.com>
2023-05-13 14:43:13 +0800

    结构型-对象-代理模式

---
+++ src/whc/design/pattern/structural/object/proxy/thought/RealSubject.java
@@ -0,0 +1,14 @@
+package whc.design.pattern.structural.object.proxy.thought;
+
+import java.util.*;
+
+/**
+ * 真实主题角色
+ */
+public class RealSubject extends Subject {
+
+    public void request() {
+        //具体业务方法
+    }
+
+}
---
+++ src/whc/design/pattern/structural/object/proxy/thought/Subject.java
@@ -0,0 +1,15 @@
+package whc.design.pattern.structural.object.proxy.thought;
+
+import java.util.*;
+
+/**
+ * 抽象主题角色
+ */
+public abstract class Subject {
+
+    /**
+     * 抽象业务方法
+     */
+    public abstract void request();
+
+}
